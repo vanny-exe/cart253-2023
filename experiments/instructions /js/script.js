999be7fbdb0f4e8c63b04c73a9bdbd9324158f6b
vanny-exe <nessaracine@icloud.com>
2023-09-09 17:12:21 -0400

    video about creating shapes

--- experiments/instructions /js/script.js
+++ experiments/instructions /js/script.js
@@ -20,7 +20,14 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+   createCanvas(500, 500);
+   
+    background(200, 200, 200);
+    ellipseMode(CORNER);
+    ellipse(250, 250, 100, 100);
+    ellipse(250, 250, 80, 80);
+    ellipse(250, 250, 60, 60);
+    ellipse(250, 250, 40, 40);
 }
 
 
