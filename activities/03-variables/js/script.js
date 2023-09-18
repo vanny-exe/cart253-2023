e24fefd102e975862073a9ea92548f211c4f38b7
vanny <nessaracine@icloud.com>
2023-09-18 17:36:07 -0400

    started learning about variables

---
+++ activities/03-variables/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Variables Lecture
+ * Vanessa Racine
+ * 
+ * Experiments and testing for lesson three: variables
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas(windowWidth,windowHeight);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+background(255, 0, 0);
+rectMode(CENTER);
+rect(mouseX, mouseY, 100, 100);
+}
