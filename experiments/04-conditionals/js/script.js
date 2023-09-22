b4488782273da2b0de2fc4726d5450fa7688da77
vanny <nessaracine@icloud.com>
2023-09-22 18:00:49 -0400

    4.4 loops

--- experiments/04-conditionals/js/script.js
+++ experiments/04-conditionals/js/script.js
@@ -8,7 +8,11 @@
 "use strict";
 let bgShade = 0;
 
-let displayCircle = false;
+let caterpillar = {
+    x:100,
+    y:250,
+    segmentSize: 50,
+}
 
 
 /**
@@ -32,11 +36,24 @@ function setup() {
 */
 function draw() {
     background(bgShade);
-   if (keyIsPressed) {
-    displayCircle = true;
-   }
-    if (displayCircle)
- {
-    ellipse(250,250,100,100);
- }
+    noStroke();
+    fill(100,200,100);
+
+    // let x = caterpillar.x;
+    // let numSegments = 8;
+    // let segmentsDrawn = 0;
+
+    // while (segmentsDrawn < numSegments) {
+    //     ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    //     x = x + 40;
+    //     segmentsDrawn ++; 
+    // }
+
+let x = caterpillar.x;
+let numSegments = 10;
+// more common way of doing above comments
+for (let segmentsDrawn=0; segmentsDrawn < numSegments; segmentsDrawn++) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 40;
+}
 }
