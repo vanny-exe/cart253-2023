55cdaa5e7ae2d7fe67a94e1a2f8d0948173d55eb
vanny <nessaracine@icloud.com>
2023-09-22 19:03:01 -0400

    4.7 movement

--- experiments/04-conditionals/js/script.js
+++ experiments/04-conditionals/js/script.js
@@ -6,15 +6,18 @@
  */
 
 "use strict";
-let bg = {
-    r: 0,
-    g: 0,
-    b: 0
-}
+
+
 let circle = {
     x: 250,
     y: 250,
-    size: 100
+    size: 100,
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ax: 0,
+    acceleration: 0.1,
+    maxSpeed: 10,
 };
 
 
@@ -40,15 +43,29 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(bg.r,bg.g,bg.b);
+    background(0);
     
-    
+    if (mouseX < circle.x) {
+        circle.ax =- circle.acceleration;
+    }
+    else {
+        circle.ax = + circle.acceleration;
+    }
+
+    if (mouseY < circle.y) {
+        circle.ay = -circle.acceleration;
+    }
+    else {
+        circle.ay = +circle.acceleration;
+    }
+
+    circle.vx = circle.vx + circle.ax;
+    circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
+    circle.vy = circle.vy + circle.ay;
+    circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
     ellipse(circle.x,circle.y,circle.size);
-
-}
-
-function mouseWheel() {
-    bg.r = random(0,255);
-    bg.g = random(0, 255);
-    bg.b = random(0,255);
 }
