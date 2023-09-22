dea8bddb83bdec944da7b431942c714d66a10858
vanny <nessaracine@icloud.com>
2023-09-22 19:28:17 -0400

    4.8 intermediate drawing

--- experiments/04-conditionals/js/script.js
+++ experiments/04-conditionals/js/script.js
@@ -44,28 +44,5 @@ function setup() {
 */
 function draw() {
     background(0);
-    
-    if (mouseX < circle.x) {
-        circle.ax =- circle.acceleration;
-    }
-    else {
-        circle.ax = + circle.acceleration;
-    }
-
-    if (mouseY < circle.y) {
-        circle.ay = -circle.acceleration;
-    }
-    else {
-        circle.ay = +circle.acceleration;
-    }
-
-    circle.vx = circle.vx + circle.ax;
-    circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
-    circle.vy = circle.vy + circle.ay;
-    circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
-
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-
-    ellipse(circle.x,circle.y,circle.size);
+    // already learned these in the last assignment oops
 }
