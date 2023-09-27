5d162a142a06298b0628a56d9196f0c6b2c43dff
vanny <nessaracine@icloud.com>
2023-09-26 21:36:00 -0400

    exercise - final touches and fixes

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -2,28 +2,52 @@
  * Exercise 3: dodge em
  * Vanessa Racine
  * 
- * A simulation of avoiding the impending void, 
+ * A simulation of avoiding the impending void and the exhaustion of avoiding friends.  
  */
 
-/** THE PLAN: 
- * 1) change the way user controls circle: 
- *      -   change to keys (done!)
- * 2) add a new if-statement: 
- *      -   if you touch the void, circle grows to fill screen 
- *      -   if circle fills screen, then game ends (implies theres a way to stop it... but thats a problem for another day)
- * 3) change the looks: 
- *     -    add more things to avoid, the void being the game-ender
- *     -    add objects that your sprite can't run into (trees? maybe? if you have time)
- * 4) use an image: swap out circle for pixel sprite, swap out covid for new variable (gitchi manitou aka manitou)*/ 
 
 
 "use strict";
 
-let userImage;
-let voidImage;
-let friendImage;
+let tree = {
+    x: 100,
+    y: 500,
+    w: 200,
+    h: 250,
+    size: 200,
+    fill: {
+        r: 255,
+        g: 255,
+        b: 255,
+    },
+    image: undefined,
+    totalSegments: 5,
+    segmentSize: 200,
+    segmentSpacing: 100,
+    
+}
 
-let user= {
+
+let space = {
+    x: 200,
+    y: 200,
+    size: 90,
+    fill: {
+        r: 148,
+        g: 105,
+        b: 72,
+    },
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ay:0,
+    maxSpeed: 2, 
+    acceleration: 0.05,
+    image: undefined, 
+};
+
+
+let player = {
     x: 350,
     y: 350,
     size: 100,
@@ -31,34 +55,41 @@ let user= {
         r: 148,
         g: 105,
         b: 72,
-    }
+    },
+    image: undefined, 
+    vx: 0,
+    vy:0,
 }
 
-let covid = {
-    x: 0,
-    y: 250,
-    size:100,
+let friend = {
+    x: 230,
+    y: 0,
+    size:80,
     vx: 0,
     vy: 0,
-    speed: 5,
+    speed: 3,
     fill: {
         r: 35,
         g: 31,
         b: 38
-    }
+    },
+    image: undefined,
 
 
 }
 let numStatic = 1000;
-    
+let move = 5;
 
 /**
  * Description of preload
 */
 function preload() {
-   userImage = loadImage("assets/images/user.png");
-   voidImage = loadImage("assets/images/void.png");
-   friendImage = loadImage("assets/images/friend.png");
+
+   player.image = loadImage("assets/images/user.png");
+   space.image = loadImage("assets/images/void.png");
+   friend.image = loadImage("assets/images/friend.png");
+   tree.image = loadImage("assets/images/tree.png");
+   
 }
 
 
@@ -66,12 +97,13 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(800,600);
+    createCanvas(600,600);
     
-    covid.y = random(0,height);
-    covid.vx = covid.speed;
+    friend.x = random(0,width);
+    friend.vy = friend.speed;
 
-    noCursor(); 
+     
+    imageMode(CENTER);
 }
 
 
@@ -79,7 +111,9 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(91,83,112);
+    background(15,9,18);
+    
+
 
 // display static
 
@@ -91,57 +125,120 @@ for (let i = 0; i < numStatic; i++) {
 }
 
 
-
-
-
-// COVID movement
-    covid.x = covid.x + covid.vx; // standard movement code
-    covid.y = covid.y + covid.vy;
-
-    if (covid.x > width) {
-        covid.x = 0;
-        covid.y = random(0,height);// if statements should go before the fill/ellipse
+// player boundaries in frame
+    if (player.x < 0)  {
+    player.x = player.x + move;
+    }
+    if (player.x > width)  {
+    player.x = player.x - move;
+    }
+    if (player.y < 0)  {
+    player.y = player.y + move;
+    }
+    if (player.y > height)  {
+    player.y = player.y - move;
     }
 
-// Check for catching covid
-let d = dist(user.x,user.y,covid.x,covid.y); // pixels between user and covid
-if (d < covid.size/2 + user.size/2) {
+    
+    
+    
+   
+// space movement - moving around player and expands/ends when touches player
+    // acceleration 
+    if (player.x > space.x) {
+        space.ax = space.acceleration;
+    }
+    else if (player.x < space.x) {
+        space.ax = -space.acceleration;
+    };
+
+    if (player.y > space.y) {
+        space.ay = space.acceleration;
+    }
+    else if (player.y < space.y) {
+        space.ay = -space.acceleration;
+    };
+    // adjusting velosity and constraining it
+    space.vx = space.vx + space.ax;
+    space.vx = constrain(space.vx, -space.maxSpeed, space.maxSpeed);
+    space.vy = space.vy + space.ay;
+    space.vy = constrain(space.vy, -space.maxSpeed, space.maxSpeed);
+    // applying the changes to space position
+    space.x = space.x + space.vx;
+    space.y = space.y + space.vy;
+    // image at position 
+    fill(space.fill.r,space.fill.g,space.fill.b);
+    image(space.image,space.x,space.y,space.size,space.size)
+
+//  catching the space void
+    let d = dist(player.x,player.y,space.x,space.y); // pixels between player and space void
+    if (d < space.size/2.2 + player.size/2.2) {
+    noLoop();
+    };
+
+// friend movement
+    friend.x = friend.x + friend.vx; // standard movement code
+    friend.y = friend.y + friend.vy;
+
+    if (friend.y > height) {
+        friend.y = 0;
+        friend.x = random(0,width);// if statements should go before the fill/ellipse
+    };
+
+// catching friend
+let d2 = dist(player.x,player.y,friend.x,friend.y); // pixels between player and friend
+if (d2 < friend.size/2.5 + player.size/2.5) {
     noLoop();
 }
 
-// User movement 
+
+// player movement 
 
     if (keyIsDown(LEFT_ARROW)) {
-      user.x -= 5;
+      player.x -= move;
     }
   
     if (keyIsDown(RIGHT_ARROW)) {
-      user.x += 5;
+      player.x += move;
     }
   
     if (keyIsDown(UP_ARROW)) {
-      user.y -= 5;
+      player.y -= move;
     }
   
     if (keyIsDown(DOWN_ARROW)) {
-      user.y += 5;
+      player.y += move;
     }
 
-//COVID display
-    fill(covid.fill.r,covid.fill.g,covid.fill.b);
-    ellipse(covid.x,covid.y,covid.size);
 
- //USER display
-    fill(user.fill.r,user.fill.g,user.fill.b);
-    ellipse(user.x,user.y,user.size);
 
-//IMAGES
-    image(userImage,0,0,80,80);
-    image(voidImage,100,100,80,80);
-    image(friendImage,200,200,80,80);
+   
+//friend display
+    fill(friend.fill.r,friend.fill.g,friend.fill.b);
+    image(friend.image,friend.x,friend.y,friend.size,friend.size);
+
+ //player display
+    fill(player.fill.r,player.fill.g,player.fill.b);
+    image(player.image, player.x,player.y,player.size, player.size);
+
+    // loop trees along x axis 
+
+let x = tree.x;
+for (let i = 0; i < tree.totalSegments; i++) {
+    image(tree.image,x, tree.y, tree.h,tree.segmentSize);
+    x = x + tree.segmentSpacing;
+}
+//tree display
+    fill(tree.fill.r,tree.fill.g,tree.fill.b);
+    image(tree.image, tree.x,tree.y,tree.size, tree.size);
+ 
+
+
+
+ 
+
 }
 
-// User Movement
 
 
 
