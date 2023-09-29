8da7b8f8e56c36becf537f402ed1c747777a2876
vanny <nessaracine@icloud.com>
2023-09-29 12:40:17 -0400

    functions 5.3

--- experiments/05-functions/js/script.js
+++ experiments/05-functions/js/script.js
@@ -21,9 +21,15 @@ function preload() {
 
 /**
  * Description of setup
-*/
+*/// why is this not working? 
 function setup() {
     createCanvas(500,500);
+
+    let hotCelsius = toCelsius(100);
+    console.log(`100 degrees fahrenheit is ${hotCelsius} degrees Celsius.`);
+
+    let coldCelsius = toCelsius(10);
+    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
 }
 
 
@@ -31,23 +37,11 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(108, 135,115);
-    parallels(125,100,20,1,200, 25);
-    parallels(200,220,5,5,70,10);
-    parallels(200,320,30,2,120,6);
-
-   
+background(0);
 
 }
-function parallels(x,y,numLines,lineWidth,lineHeight, lineSpacing) {
-    //let x = 50; we dont need these variables when we have (x,y) in the function
-    //let y = 250;
-    for (let i = 0; i < numLines; i++) {
-        noStroke();
-        fill(212, 193, 227);
-        rectMode(CENTER);
-        rect(x,y,lineWidth,lineHeight);
-        x = x+ lineSpacing;
-    }
-}
 
+function toCelsius(fahrenheight){
+    let celsius = (fahrenheight-32) * 5/9;
+    return celsius; 
+}
