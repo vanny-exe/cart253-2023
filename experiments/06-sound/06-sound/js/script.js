0bf2954cd372ff1ba09ba0002950cf4daa0b4386
vanny <nessaracine@icloud.com>
2023-10-10 14:04:34 -0400

    started sound lesson

---
+++ experiments/06-sound/06-sound/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * lesson 5 - sound
+ * v-racine
+ * 
+ * rundown of lesson 5 video 
+ */
+
+"use strict";
+let barkSFX; 
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
