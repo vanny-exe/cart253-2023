a8461d9db817b66dd578d7f99ac76e4182f9ed07
vanny <nessaracine@icloud.com>
2023-11-02 20:15:44 -0400

    learnin

---
+++ experiments/08 - object oriented programming/js/Flower.js
@@ -0,0 +1,45 @@
+class Flower {
+    constructor(x, y, size, stemLength, petalColor) {
+            // Position and size information
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.stemLength = stemLength;
+        this.stemThickness = 10;
+        this.petalThickness = 10;
+            // Color information
+        this.stemColor = {
+              r: 50,
+              g: 150,
+              b: 50
+            };
+        this.petalColor = petalColor;
+        this.centreColor = {
+              r: 50,
+              g: 0,
+              b: 0
+            };
+        }
+
+    display() {
+        push();
+            // Draw a line for the stem
+        strokeWeight(this.stemThickness);
+        stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+        line(this.x, this.y, this.x, this.y + this.stemLength);
+            // Draw a circle with a heavy outline for the flower
+        strokeWeight(this.petalThickness);
+        fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+        stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+        ellipse(this.x, this.y, this.size);
+        pop();
+        }
+
+    mousePressed() {
+        let d = dist(this.x, this.y, mouseX, mouseY);
+        if (d < this.size/2 + this.petalThickness) {
+            this.stemLength = this.stemLength + 5;
+            this.y = this.y - 5;
+        }
+    }
+}
---
+++ experiments/08 - object oriented programming/js/script.js
@@ -0,0 +1,76 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+// Our garden
+let garden = {
+    // An array to store the individual flowers
+    flowers: [],
+    // How many flowers in the garden
+    numFlowers: 20,
+    // The color of the grass (background)
+    grassColor: {
+      r: 120,
+      g: 180,
+      b: 120
+    }
+  };
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(600, 600);
+  
+    // Create our flowers by counting up to the number of the flowers
+    for (let i = 0; i < garden.numFlowers; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        let size = random(50,80);
+        let stemLength = random(50,100);
+        let petalColor = {
+            r: random(100,255),
+            g: random(100,255),
+            b: random(100,255)
+        }
+      // NEW! Create a new flower
+      let flower = new Flower(x, y, size, stemLength, petalColor);
+      // Add the flower to the array of flowers
+      garden.flowers.push(flower);
+    }
+  }
+  
+
+/**
+ * Description of draw()
+*/
+function draw() {
+   // Display the grass
+   background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+   // Loop through all the flowers in the array and display them
+   for (let i = 0; i < garden.flowers.length; i++) {
+     let flower = garden.flowers[i];
+     flower.display();
+   }
+ }
+
+function mousePressed() {
+    for (let i = 0 < garden.flowers.length; i++;) {
+        let flower = garden.flowers[i];
+        flower.mousePressed();
+    }
+}
